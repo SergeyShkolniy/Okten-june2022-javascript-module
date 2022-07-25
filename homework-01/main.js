1cc4f08b53f783ae94bf95b10c96eceafc431239
SergeyShkolniy <s.shkolniy@gmail.com>
2022-07-25 17:01:34 +0300

    homework-01

---
+++ homework-01/main.js
@@ -0,0 +1,75 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+
+// let hi = 'hello';
+// let company = 'owu';
+// let web = 'com';
+// let country = 'ua';
+// let num1 = 1;
+// let num10 = 10;
+// let num999 = -999;
+// let num123 = 123;
+// const pi = 3.14;
+// let num27 = 2.7;
+// let num16 = 16;
+// let iAmDeveloper = true;
+// let iAmPilot = false;
+
+// Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// console.log(hi);
+// console.log(company);
+// console.log(web);
+// console.log(country);
+// console.log(num1);
+// console.log(num10);
+// console.log(num999);
+// console.log(num123);
+// console.log(pi);
+// console.log(num27);
+// console.log(num16);
+// console.log(iAmDeveloper);
+// console.log(iAmPilot);
+
+// alert(hi);
+// alert(company);
+// alert(web);
+// alert(country);
+// alert(num1);
+// alert(num10);
+// alert(num999);
+// alert(num123);
+// alert(pi);
+// alert(num27);
+// alert(num16);
+// alert(iAmDeveloper);
+// alert(iAmPilot);
+
+// document.write(`${hi} ${company} ${web} ${country} ${num1} ${num10} ${num999} ${num123} ${pi} ${num27} ${num16} ${iAmDeveloper} ${iAmPilot} `);
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+// let firstName = 'Sergey ';
+// let middleName = 'Aleksandrovich';
+// let lastName = 'Shkolniy ';
+// let person = (lastName + firstName + middleName);
+// console.log(person);
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
+
+// let a = 100;
+// console.log(typeof a);
+//
+// let b = '100';
+// console.log(typeof b);
+//
+// let c = true;
+// console.log(typeof c);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+// let firstName = prompt('Type your name');
+// let middleName = prompt('Type your middle name');
+// let age = prompt('Type your age');
+//
+// console.log(`Hello  ${firstName}  ${middleName}. You ${age} years!`)
